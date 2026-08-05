aea88d619fc4050515c17a3bde7ddc99713e8e11
iPlusmsk07 <iplusmsk07@icloud.com>
2026-08-04 21:58:37 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Публичная конфигурация Supabase. Этот файл можно хранить в GitHub.
 window.POMOGAY_CONFIG = {
   SUPABASE_URL: 'https://llnjgyehxsogjmwegnyf.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_Dhd8B3QTpaSNJeTNjkToJg_rPOAgLMJ'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbmpneWVoeHNvZ2ptd2VnbnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3ODY1MjksImV4cCI6MjEwMTM2MjUyOX0.u90AmCm6QC_C4lz0ul6a_TibS6maSpZ2gK6RQuwzFXI'
 };
